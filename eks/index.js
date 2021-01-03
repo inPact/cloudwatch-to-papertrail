501764081a0ae252eea5d6c757fd19a3258b1d0e
royb_tabit <roy@tabit.cloud>
2021-01-03 18:28:50 +0200

    update

--- eks/index.js
+++ eks/index.js
@@ -138,11 +138,17 @@ exports.handler = function (event, context, cb) {
     data.logEvents.forEach(function (line) {
 
       var obj = JSON.parse(line.message)
-
       //if ( obj.kubernetes.container_name == 'k8-internus' || obj.kubernetes.container_name == 'ros-api-dev'){
-      // if ( obj.kubernetes.container_image.startsWith('075139435924.dkr.ecr.eu-west-1.amazonaws.com')){
-      log.info( obj.log);
-      // }
+      if ( obj.kubernetes == undefined ) {
+        //fargate logs
+        log.info( obj.log.substr(40) );
+      }
+      else {
+        //ordinary logs
+        if ( obj.kubernetes.container_image.startsWith('075139435924.dkr.ecr.eu-west-1.amazonaws.com')){
+          log.info( obj.log);
+        }
+      }
 
       if (config.datadog !== '') {
       //  var metricMatch = line.log.trim().match(metricRegex);
