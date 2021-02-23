df015a33b0cf1a5ee3a62075943242b67943e527
royb_tabit <roy@tabit.cloud>
2021-02-23 13:53:55 +0200

    update

--- ecs-to-elk/index.js
+++ ecs-to-elk/index.js
@@ -13,7 +13,7 @@ this.client = new Client({
 function sendData(data) {
   data.logEvents.forEach(function (line) {
     client.index({
-      index: ELK_INDEX
+      index: config.index;
       body: {
         ['@timestamp']: (new Date()).toISOString(),
         message: line.message,
